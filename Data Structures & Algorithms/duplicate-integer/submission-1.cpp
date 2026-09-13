a326a82172d3363713077e454730b2c1f0036c07
Anshika Tomar <tanshika240@gmail.com>
2026-09-13 20:57:34 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        if(nums.empty()){
+            return false;
+        }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                continue;
+            }
+            else if(nums[i]==nums[i+1]){
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};
