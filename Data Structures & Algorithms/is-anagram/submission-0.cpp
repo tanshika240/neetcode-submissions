07707d61e7cd5310a378ea024b419dea866882dc
Anshika Tomar <tanshika240@gmail.com>
2026-09-20 16:40:35 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int>mp;
+        unordered_map<char,int>mp2;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            mp2[t[i]]++;
+        }
+        for(auto x:mp){
+            if(mp2[x.first]!=x.second){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
